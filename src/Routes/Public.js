975c9955777bf1d9399765fcf3bc804bef79a583
Alejandro Spitale <alejandro.spitale@outlook.com>
2023-03-31 23:49:23 -0300

    This is a click game init

--- src/Routes/Public.js
+++ src/Routes/Public.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import MainPage from '../pages/MainPage';
 import '../styles/Global.css';
 
 function Public() {
   return (
     <>
-      <div
-        className='contain'
-        style={{ display: 'block', margin: '0 auto', padding: '0' }}
-      >
-        <Routes>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/main' element={<MainPage />}></Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+      </Routes>
     </>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import NameForm from '../components/NameForm';
 
 function HomePage() {
-  return (
-    <div className='main-container'>
-      Welcome to SPITALAN Project
-      <NameForm />
-    </div>
-  );
+  return <div>HomePage</div>;
 }
 
 export default HomePage;
